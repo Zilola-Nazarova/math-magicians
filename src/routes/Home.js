ab9747ea6ac0f83512db641cbceeb4c2c582c725
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-08-18 02:01:20 +0500

    Style Home route's components

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,32 @@
+import styles from '../styles/Home.module.css';
+
 const Home = () => (
-  <div className="welcome">
-    <h1>Hi</h1>
-    <p>Welcome</p>
-    <p>Welcome</p>
+  <div className={styles.home}>
+    <h2>Welcome to our page!</h2>
+    <p>
+      The body of knowledge and practice known as mathematics is derived from the
+      contributions of thinkers throughout the ages and across the globe. It gives
+      us a way to understand patterns, to quantify relationships, and to predict
+      the future. Math helps us understand the world — and we use the world to
+      understand math.
+    </p>
+    <br />
+    <p>
+      The world is interconnected. Everyday math shows these connections
+      and possibilities. The earlier young learners can put these skills
+      to practice, the more likely we will remain an innovation society
+      and economy.
+    </p>
+    <br />
+    <p>
+      Algebra can explain how quickly water becomes contaminated and how many
+      people in a third-world country drinking that water might become sickened
+      on a yearly basis. A study of geometry can explain the science behind
+      architecture throughout the world. Statistics and probability can estimate
+      death tolls from earthquakes, conflicts and other calamities around the world.
+      It can also predict profits, how ideas spread, and how previously endangered
+      animals might repopulate.
+    </p>
   </div>
 );
 
